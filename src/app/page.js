c3dba671823c22bc36977f2ed75436dd77a3db24
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-03 04:24:51 +0530

    Events , functions and state, even use client

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,89 @@
-import Image from 'next/image';
+'use client';
+import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
+  const [name, setName] = useState('Jignesh');
+  // const apple = () => {
+  //   alert('Apple is a Fruit');
+  // };
+
+  const apple = (item) => {
+    alert(`Apple is a ${item}`);
+  };
+
+  const updateName = () => {
+    setName('Jignesh Sharma');
+  };
+
+  const InnerComponent = () => {
+    return <div>Inner Component</div>;
+  };
+
   return (
     <main className={styles.main}>
-      <h2>Hello</h2>
-      {/* Used resuable components and passed data to components */}
-      <User name="Jignesh Sharma" />
-      <User name="Ashish Jain" />
-      <User name="Raju Bhai" />
+      <h2>Events Functions and State {name}</h2>
+      {/* <button
+        onClick={() => alert('Button Clicked')}
+        style={{
+          padding: '10px 30px',
+          background: 'black',
+          color: '#fff',
+          fontSize: '18px',
+          outline: 'none',
+          border: 'none',
+          borderRadius: '10px',
+        }}
+      >
+        Click
+      </button> */}
+
+      {/* <button
+        onClick={apple}
+        style={{
+          padding: '10px 30px',
+          background: 'black',
+          color: '#fff',
+          fontSize: '18px',
+          outline: 'none',
+          border: 'none',
+          borderRadius: '10px',
+        }}
+      >
+        Click
+      </button> */}
+
+      {/* <button
+        onClick={() => apple('Fruit')}
+        style={{
+          padding: '10px 30px',
+          background: 'black',
+          color: '#fff',
+          fontSize: '18px',
+          outline: 'none',
+          border: 'none',
+          borderRadius: '10px',
+        }}
+      >
+        Click
+      </button> */}
+
+      <button
+        onClick={updateName}
+        style={{
+          padding: '10px 30px',
+          background: 'black',
+          color: '#fff',
+          fontSize: '18px',
+          outline: 'none',
+          border: 'none',
+          borderRadius: '10px',
+        }}
+      >
+        Click
+      </button>
+
+      <InnerComponent />
     </main>
   );
 }
-
-const User = ({ name }) => {
-  return (
-    <div>
-      <h3>
-        User name is:
-        <span style={{ color: 'grey', fontWeight: 'bold' }}>{name}</span>{' '}
-      </h3>
-    </div>
-  );
-};
