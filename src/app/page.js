1d1deedcbd9965ebc593d1ef24147dd971722212
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-03 04:03:41 +0530

    make a component and resused it and passed data

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h2>Hello</h2>
+      {/* Used resuable components and passed data to components */}
+      <User name="Jignesh Sharma" />
+      <User name="Ashish Jain" />
+      <User name="Raju Bhai" />
     </main>
   );
 }
+
+const User = ({ name }) => {
+  return (
+    <div>
+      <h3>
+        User name is:
+        <span style={{ color: 'grey', fontWeight: 'bold' }}>{name}</span>{' '}
+      </h3>
+    </div>
+  );
+};
