f4027196027bf67f41a210b468ef56ca38a34ed6
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-03 06:46:40 +0530

    nesting routing

---
+++ src/app/about/aboutcollege/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+const AboutCollege = () => {
+  return (
+    <div>
+      <h2>AboutCollege</h2>
+      <br />
+      <Link href="/about">Go to about page</Link>
+    </div>
+  );
+};
+
+export default AboutCollege;
---
+++ src/app/about/aboutstudent/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+const AboutStudent = () => {
+  return (
+    <div>
+      <h2>AboutStudent</h2>
+      <br />
+      <Link href="/about">Go to about page</Link>
+    </div>
+  );
+};
+
+export default AboutStudent;
