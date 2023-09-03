18b0bcd28e54d9daab9a6ad93a2316aa10df8daa
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-03 08:05:30 +0530

    404 page and for a specific route page

---
+++ src/app/login/[...not-found]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPageNotFound = () => {
+  return (
+    <div>
+      <h3>This login page not available</h3>
+    </div>
+  );
+};
+
+export default LoginPageNotFound;
