8434f85884ef248c2efa19f2f6c28b0eeaf92487
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-04 02:14:51 +0530

    api-fetching in client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,27 @@
-'use client';
-
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  const navigate = (routeName) => {
-    router.push(routeName);
-  };
   return (
-    <main>
-      <h2>Basic Routing</h2>
-      <br />
-      {/* Linking */}
-      <Link href="/login">Go to login page</Link>
-      <br />
-      <br />
-      <Link href="/about">Go to about page</Link>
-      {/* Linking */}
-      <br />
-      {/* Navigation */}
-      <div style={{ marginTop: '30px' }}>
-        <button
-          style={{
-            padding: '10px 30px',
-            background: 'black',
-            marginLeft: '2px',
+    <>
+      <main
+        style={{
+          textAlign: 'center',
+          padding: '10px 0px',
+          border: '1px solid black',
+        }}
+      >
+        <h1>Fetch Data with API in Client Component</h1>
+      </main>
 
-            color: '#fff',
-            fontSize: '18px',
-            outline: 'none',
-            border: 'none',
-            borderRadius: '2px',
-            cursor: 'pointer',
-          }}
-          onClick={() => navigate('/login')}
-        >
-          Go to login page
-        </button>
-
-        <button
-          style={{
-            cursor: 'pointer',
-            padding: '10px 30px',
-            marginLeft: '20px',
-            background: 'black',
-            color: '#fff',
-            fontSize: '18px',
-            outline: 'none',
-            border: 'none',
-            borderRadius: '2px',
-          }}
-          onClick={() => navigate('/about')}
-        >
-          Go to about page
-        </button>
-      </div>
-    </main>
+      <section
+        style={{
+          textAlign: 'center',
+          marginTop: '20px',
+          fontSize: '30px',
+        }}
+      >
+        <Link href="/productlist">Go to product page</Link>
+      </section>
+    </>
   );
 }
