c91c2479bafd0559f27129989ddfd8f01ce8c08e
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-03 07:23:14 +0530

    conditional-layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,34 +1,45 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Layout({ children }) {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <div>
-      <div
-        style={{
-          padding: '30px 100px',
-          display: 'flex',
-          justifyContent: 'space-between',
-          backgroundColor: 'lightsalmon',
-        }}
-      >
-        <Link href="/login">
-          <h2>Login Page</h2>
-        </Link>
-
-        <ul
+      {pathname !== '/login/loginteacher' ? (
+        <div
           style={{
+            padding: '30px 100px',
             display: 'flex',
-            listStyle: 'none',
-            columnGap: '40px',
+            justifyContent: 'space-between',
+            backgroundColor: 'lightsalmon',
           }}
         >
-          <li>
-            <Link href="/login/loginteacher">Login Teacher</Link>
-          </li>
-          <Link href="/login/loginstudent">Login Student</Link>
-          <li></li>
-        </ul>
-      </div>
+          <Link href="/login">
+            <h2>Login Page</h2>
+          </Link>
+
+          <ul
+            style={{
+              display: 'flex',
+              listStyle: 'none',
+              columnGap: '40px',
+            }}
+          >
+            <li>
+              <Link href="/login/loginteacher">Login Teacher</Link>
+            </li>
+            <Link href="/login/loginstudent">Login Student</Link>
+            <li></li>
+          </ul>
+        </div>
+      ) : (
+        <Link href="/login" style={{ textAlign: 'center' }}>
+          Back to login page
+        </Link>
+      )}
       {children}
     </div>
   );
