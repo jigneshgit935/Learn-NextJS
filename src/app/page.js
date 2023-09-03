3d8417c5782faad96e70423444cfe2563dafc17a
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-03 06:18:45 +0530

    basic routing in nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,89 +1,10 @@
-'use client';
-import { useState } from 'react';
+import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  const [name, setName] = useState('Jignesh');
-  // const apple = () => {
-  //   alert('Apple is a Fruit');
-  // };
-
-  // const apple = (item) => {
-  //   alert(`Apple is a ${item}`);
-  // };
-
-  const updateName = () => {
-    setName('Jignesh Sharma');
-  };
-
-  const InnerComponent = () => {
-    return <div>Inner Component</div>;
-  };
-
   return (
     <main className={styles.main}>
-      <h2>Events Functions and State {name}</h2>
-      {/* <button
-        onClick={() => alert('Button Clicked')}
-        style={{
-          padding: '10px 30px',
-          background: 'black',
-          color: '#fff',
-          fontSize: '18px',
-          outline: 'none',
-          border: 'none',
-          borderRadius: '10px',
-        }}
-      >
-        Click
-      </button> */}
-
-      {/* <button
-        onClick={apple}
-        style={{
-          padding: '10px 30px',
-          background: 'black',
-          color: '#fff',
-          fontSize: '18px',
-          outline: 'none',
-          border: 'none',
-          borderRadius: '10px',
-        }}
-      >
-        Click
-      </button> */}
-
-      {/* <button
-        onClick={() => apple('Fruit')}
-        style={{
-          padding: '10px 30px',
-          background: 'black',
-          color: '#fff',
-          fontSize: '18px',
-          outline: 'none',
-          border: 'none',
-          borderRadius: '10px',
-        }}
-      >
-        Click
-      </button> */}
-
-      <button
-        onClick={updateName}
-        style={{
-          padding: '10px 30px',
-          background: 'black',
-          color: '#fff',
-          fontSize: '18px',
-          outline: 'none',
-          border: 'none',
-          borderRadius: '10px',
-        }}
-      >
-        Click
-      </button>
-
-      <InnerComponent />
+      <h2>Basic Routing</h2>
     </main>
   );
 }
