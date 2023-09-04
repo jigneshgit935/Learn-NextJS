618f08caf11a9d0959df35b8c57ad14ee1c2011b
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-04 07:23:01 +0530

    font-optimization in nextjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,12 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300&display=swap"
-        rel="stylesheet"
-      ></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
