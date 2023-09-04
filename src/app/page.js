fc6a0b2f481f20cf521e755afa2f4e77272c2939
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-04 07:37:41 +0530

    generate metadata

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,3 @@
-import { Roboto } from 'next/font/google';
-
-const roboto = Roboto({
-  weight: '100',
-  subsets: ['latin'],
-  display: 'swap',
-});
 export default function Home() {
   return (
     <>
@@ -15,7 +8,7 @@ export default function Home() {
           border: '1px solid black',
         }}
       >
-        <h1 className={roboto.className}>Font Optimization</h1>
+        <h1>Generate Metadata</h1>
       </main>
     </>
   );
